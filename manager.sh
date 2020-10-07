204cb24a471e018d4c36d8b29d3d89d2a8d64bd7
jameskeywood <jskeywood@gmail.com>
2020-10-07 21:48:47 +0100

    added features, along with bugs

--- manager.sh
+++ manager.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
-dialog --inputbox "Enter your name:" 8 40 2>>name.txt
+function menu {
+
+
+choice=$(
+
+	dialog	--menu "Choose one" 0 0 0 \
+		Select "- Select a preset" \
+		Add "- Add a preset" \
+		Remove "- Remove a preset" \
+		View "- View a preset" )
+
+case "$choice" in
+	"Select")
+		echo help
+		;;
+	"Add")
+		echo nou
+		;;
+	"Remove")
+		echo hehe
+		;;
+	"View")
+		echo lmao
+		;;
+	*)
+		echo somethingeles
+esac
+
+}
+
+menu
+
+exit 0
