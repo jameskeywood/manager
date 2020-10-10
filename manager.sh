fb5a2cf5fe4a7ebdd04ca82b54680cbe3189b0a6
jameskeywood <jskeywood@gmail.com>
2020-10-10 19:40:27 +0100

    added features

--- manager.sh
+++ manager.sh
@@ -2,18 +2,23 @@
 
 function menu {
 
-
-choice=$(
-
-	dialog	--menu "Choose one" 0 0 0 \
-		Select "- Select a preset" \
-		Add "- Add a preset" \
-		Remove "- Remove a preset" \
-		View "- View a preset" )
+choice=$(dialog	--clear \
+				--menu "Choose one" 0 0 0 \
+					"Select" "Select a preset" \
+					"Add" "Add a preset" \
+					"Remove" "Remove a preset" \
+					"View" "View a preset" \
+				--output-fd 1)
 
 case "$choice" in
 	"Select")
-		echo help
+		index=($(cat config/index.txt))
+		preset=$(dialog --clear \
+						--menu "Select" 0 0 0 \
+							"${index[@]}" \
+						--output-fd 1)
+		items=($(cat config/presets/"$preset.txt"))
+		xwallpaper --zoom "${items[0]}"
 		;;
 	"Add")
 		echo nou
@@ -22,10 +27,13 @@ case "$choice" in
 		echo hehe
 		;;
 	"View")
-		echo lmao
+		index=($(cat config/index.txt))
+		preset=$(dialog --clear \
+						--menu "Select" 0 0 0 \
+							"${index[@]}" \
+						--output-fd 1)
+		dialog --textbox config/presets/"$preset.txt" 0 0
 		;;
-	*)
-		echo somethingeles
 esac
 
 }
