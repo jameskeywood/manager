0a4fd3eb838faf46bcead104ecfee11e8a6a484e
jameskeywood <jskeywood@gmail.com>
2020-10-13 21:08:33 +0100

    added more function and error handling

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # manager
-A tool to create, save and load visual settings onto your machine. This is made for an Arch Linux install running Xorg and suckless software..
+A tool to create, save and load visual settings onto your machine. This is made for an Arch Linux install running Xorg and suckless software.
+
+This tool handles:
+- Wallpapers
--- manager.sh
+++ manager.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Visual setting manager
+# James Keywood - 13/Oct/2020
+
 function menu {
 
+# opens a dialog menu to take a user choice
 choice=$(dialog	--clear \
 				--menu "Choose one" 0 0 0 \
 					"Select" "Select a preset" \
@@ -10,29 +14,70 @@ choice=$(dialog	--clear \
 					"View" "View a preset" \
 				--output-fd 1)
 
+# initiates a new dialog box depending on option
 case "$choice" in
+
+	# allow's the user to select a preset
 	"Select")
-		index=($(cat config/index.txt))
-		preset=$(dialog --clear \
+		index=($(cat config/index.txt)) # array for index file
+		# creates dialog menu for the user to select their preset
+		preset=$(dialog	--clear \
 						--menu "Select" 0 0 0 \
 							"${index[@]}" \
 						--output-fd 1)
-		items=($(cat config/presets/"$preset.txt"))
-		xwallpaper --zoom "${items[0]}"
+		items=($(cat config/presets/"$preset.txt")) # array for config file
+		xwallpaper --zoom "${items[0]}" # sets the wallpaper
 		;;
+
+	# allow's the user to add a preset
 	"Add")
-		echo nou
+		# creates dialog menus for the user to add preset details
+		name=$(dialog	--clear \
+						--inputbox "Enter a name" 0 0 \
+						--output-fd 1)
+		name=$(echo -e "$name" | tr -d '[:space:]') # removes whitespace
+		desc=$(dialog	--clear \
+						--inputbox "Enter a description" 0 0 \
+						--output-fd 1)
+		desc=$(echo -e "$desc" | tr -d '[:space:]') # removes whitespace
+		path=$(dialog	--clear \
+						--inputbox "Enter a wallpaper path" 0 0 \
+						--output-fd 1)
+		path=$(echo -e "$path" | tr -d '[:space:]') # removes whitespace
+		echo -e "$name" >> config/index.txt
+		echo -e "$desc" >> config/index.txt
+		touch config/presets/"$name.txt"
+		echo -e "$path" >> config/presets/"$name.txt"
+
 		;;
+
+	# allow's the user to remove a preset
 	"Remove")
-		echo hehe
-		;;
+			index=($(cat config/index.txt)) # array for index file
+			# creates dialog menu for the user to select the preset to remove
+			preset=$(dialog	--clear \
+							--menu "Remove" 0 0 0 \
+								"${index[@]}" \
+							--output-fd 1)
+			# remove preset and description from index file
+			sed -i "/$preset/,+1 d" config/index.txt
+			rm config/presets/"$preset.txt" # remove config file
+			;;
+
+	# allow's the user to view a preset config file
 	"View")
-		index=($(cat config/index.txt))
-		preset=$(dialog --clear \
-						--menu "Select" 0 0 0 \
+		index=($(cat config/index.txt)) # array for index file
+		# creates dialog menu for the user to select the preset to view
+		preset=$(dialog	--clear \
+						--menu "View" 0 0 0 \
 							"${index[@]}" \
 						--output-fd 1)
+		# creates dialog textbox for the user to view the config file
 		dialog --textbox config/presets/"$preset.txt" 0 0
+		;;
+
+	*)
+		echo exit
 		;;
 esac
 
